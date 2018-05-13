86e7d7b479a8b555d89a375e663f84639a2a0055
Mikko Keinänen <mikko.keinanen@gmail.com>
2018-05-13 20:16:09 +0300

    108. More On Boilerplate Setup - Added firebase authentication

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyBoT78sVr0IZGNLxrh2_igIXjAkBnjWLao",
+            authDomain: "manager-ec9c7.firebaseapp.com",
+            databaseURL: "https://manager-ec9c7.firebaseio.com",
+            projectId: "manager-ec9c7",
+            storageBucket: "manager-ec9c7.appspot.com",
+            messagingSenderId: "491838419180"
+        };
+        firebase.initializeApp(config);
+    }
     render() {
         return(
             // provider tag needs an instance of a redux store
+            // also need a default reducer 
             <Provider store={createStore(reducers)}>
                 <View>
                     <Text>
