9dadebe4361ce88dac68d9bd3822739f6d03856e
Mikko Keinänen <mikko.keinanen@gmail.com>
2018-05-14 21:41:41 +0300

    110. Rebuilding the Login Form - added Login Form boilerplate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
     componentWillMount() {
@@ -22,11 +23,7 @@ class App extends Component {
             // provider tag needs an instance of a redux store
             // also need a default reducer 
             <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
+                <LoginForm />
             </Provider>        
         );
     }
