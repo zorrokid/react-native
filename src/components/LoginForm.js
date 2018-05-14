be014a7c97d9aa946a629b630aa5ab63dc157994
Mikko Keinänen <mikko.keinanen@gmail.com>
2018-05-14 21:45:02 +0300

    110. Rebuilding the Login Form - added added input sections and login button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
     render() {
         return (
             <Card>
+                <CardSection>
+                    <Input
+                        label="Email"
+                        placeholder="email@email.com"
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        label="Password"
+                        placeholder="password"
+                    />
+                </CardSection>
+                <CardSection>
+                    <Button>
+                        Login
+                    </Button>
+                </CardSection>
             </Card>
         );
     }
