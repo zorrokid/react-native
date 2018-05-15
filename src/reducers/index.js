8c3a3179ee1f8f950c39bac7137a385fc3367176
Mikko Keinänen <mikko.keinanen@gmail.com>
2018-05-15 22:20:27 +0300

    112. Wiring Up Creators - Added AuthReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
+import AuthReducer from './AuthReducer';
 
 export default combineReducers({
-    // dummy reducer for now:
-    banana: () => []
+    auth: AuthReducer
 });
