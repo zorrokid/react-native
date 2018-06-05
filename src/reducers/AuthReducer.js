11bc5b55ade6aee5bc9e5e4cad155637d1d6dc06
Mikko Keinänen <mikko.keinanen@gmail.com>
2018-06-05 21:47:44 +0300

    115. Creating immutable state

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -4,6 +4,13 @@ const INITIAL_STATE = { email: '' }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
+        case EMAIL_CHANGED:
+            // Make a new object with properties from state and overwrite email-property.
+            // Since we make a new object, redux-knows the state has changed.
+            // If we just update the property from original state (the state object is same), 
+            // it's not registered as changed state.
+            console.log(action.payload);
+            return { ...state, email: action.payload };
         default:
             return state;
     }
