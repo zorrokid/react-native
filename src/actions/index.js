290bcf9c3875287153a6142b94035fd9bcc0b11e
Mikko Keinänen <mikko.keinanen@gmail.com>
2018-06-06 21:24:05 +0300

    118. Introduction to Redux Thunk - Added Redux Thunk and Firebase authentication

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,8 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import firebase from 'firebase';
+import { 
+    EMAIL_CHANGED, 
+    PASSWORD_CHANGED 
+} from './types';
 
 export const emailChanged = (text) => {
     return {
@@ -13,3 +17,9 @@ export const passwordChanged = (text) => {
         payload: text
     };
 };
+
+export const loginUser = ({ email, password }) => {
+    firebase.auth().signInAndRetrieveDataWithEmailAndPassword(email, password)
+        .then(user => console.log(user));
+
+}
